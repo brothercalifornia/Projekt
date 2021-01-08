f86b605c452d253a765433721caf8942e12b5bb9
brothercalifornia <75046435+brothercalifornia@users.noreply.github.com>
2021-01-09 00:53:56 +0100

    Create Kolko.h

---
+++ Kolko.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+class Kolko {
+public:
+	void plansza(char t[]);
+	bool wygrana(char t[], char g);
+	bool remis(char t[]);
+	void ruch(char t[], char& gracz);
+	void kolko();
+};
