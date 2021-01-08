caafa2fe6da5753d32df3c8c4367888b019d463b
brothercalifornia <75046435+brothercalifornia@users.noreply.github.com>
2021-01-09 00:55:05 +0100

    Create Totolotek.h

---
+++ Totolotek.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+
+class Totolotek {
+public:
+	void los(int ilosc, int zakres);
+	void losowanie();
+};
